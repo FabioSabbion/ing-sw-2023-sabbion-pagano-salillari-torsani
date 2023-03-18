de17faef06e2195a245e3991bbf6c21beb3323e6
Lorenzo Torsani <lorenzo.torsani@mail.polimi.it>
2023-03-18 11:14:39 +0100

    Added Javadoc comments to Tile and Player

--- src/main/java/it/polimi/ingsw/models/Player.java
+++ src/main/java/it/polimi/ingsw/models/Player.java
@@ -1,18 +1,30 @@
 package it.polimi.ingsw.models;
 
+/**
+ * Represents one player in the game
+ */
 public class Player {
     private String nickname;
     private PersonalGoalCard personalGoalCard;
     private Bookshelf bookshelf;
 
+    /**
+     * @return the <b>Nickname</b> of the player
+     */
     public String getNickname() {
         return nickname;
     }
 
+    /**
+     * @return the <b>Personal Goal Card</b> of the player
+     */
     public PersonalGoalCard getPersonalGoalCard() {
         return personalGoalCard;
     }
 
+    /**
+     * @return the <b>Bookshelf</b> of the player
+     */
     public Bookshelf getBookshelf() {
         return bookshelf;
     }
