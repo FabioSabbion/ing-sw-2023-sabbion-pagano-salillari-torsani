e454d63892cec0565a94e02441536aa894a167d0
Fabio Sabbion <fabio.sabbion@mail.polimi.it>
2023-03-21 19:19:23 +0100

    Implemented basic GameController

---
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.models.*;
+import it.polimi.ingsw.models.exceptions.NotEnoughCellsException;
+import it.polimi.ingsw.models.exceptions.PickTilesException;
+import org.apache.commons.lang.NotImplementedException;
+
+import java.util.List;
+
+public class GameController {
+    private Game game;
+
+
+    public GameController(Player[] players) {
+        this.game = Game.emptyGame(players, selectCommonGoalCards());
+        this.play();
+    }
+
+    private void play() {
+
+    }
+
+    private void playerAction(List<Coordinates> coordinatesList, int column) {
+        try {
+            List<Tile> tiles = this.game.getLivingRoom().chooseTiles(coordinatesList);
+            this.game.getCurrentPlayer().getBookshelf().insertTiles(column, tiles);
+            this.game.getLivingRoom().removeTiles(tiles);
+        } catch (PickTilesException | NotEnoughCellsException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    private CommonGoalCard[] selectCommonGoalCards() {
+        throw new NotImplementedException();
+    }
+}
