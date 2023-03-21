21e9db9aefc4817435752148c7e19b05164c23ac
Andri Salillari <andri.salillari@mail.polimi.it>
2023-03-21 15:24:40 +0100

    Created LivingRoomTest

---
+++ src/test/java/it/polimi/ingsw/models/LivingRoomTest.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class LivingRoomTest {
+    private LivingRoom livingRoom;
+
+    @Test
+    @DisplayName("")
+    public void chooseTilesImpossibleStates() {
+
+    }
+
+    @BeforeEach
+    public void setup() {
+       livingRoom = new LivingRoom();
+    }
+}
