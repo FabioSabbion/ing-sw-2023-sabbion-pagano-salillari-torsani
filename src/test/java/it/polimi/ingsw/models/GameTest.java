8d04086bd154e9cdd62734ce11bc2ac01b00779b
Fabio Sabbion <fabio.sabbion@mail.polimi.it>
2023-03-21 14:57:00 +0100

    Reimplemented Game tests

--- src/test/java/it/polimi/ingsw/models/GameTest.java
+++ src/test/java/it/polimi/ingsw/models/GameTest.java
@@ -1,66 +1,34 @@
 package it.polimi.ingsw.models;
 
-import it.polimi.ingsw.models.exceptions.NotEnoughCellsException;
-import it.polimi.ingsw.models.exceptions.PickTilesException;
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
     private Game game;
-    private int[] personalGoalCardScores = new int[]{0,1,2,4,6,9,12};
-
     @Test
-    void getScoreBoard() {
-        Map<Player, Integer> scoreboard = game.getScoreBoard();
-        for (Player p: game.getPlayers()) {
-            int pScore = 0;
-            if (p == game.getGameEnder())
-                pScore += 1;
-            for (CommonGoalCard cgc: game.getCommonGoalCards())
-                pScore += cgc.checkGoal(p);
-            List<Pair<Category, Integer>> closeTiles = p.getBookshelf().getCloseTiles();
-            for (int n: closeTiles.stream().mapToInt(Pair::getRight).toArray()) {
-                if (n == 3)
-                    pScore += 2;
-                if (n == 4)
-                    pScore += 3;
-                if (n == 5)
-                    pScore += 5;
-                if (n >= 6)
-                    pScore += 8;
-            }
-            int count = 0;
-            for (Pair<Category, Coordinates> pair: p.getPersonalGoalCard().getPositions()) {
-                if (p.getBookshelf().getBookshelf()[pair.getRight().x][pair.getRight().y].getCategory() == pair.getLeft())
-                    count++;
-            }
-            pScore += personalGoalCardScores[count];
-            assertEquals(pScore, scoreboard.get(p));
-        }
-    }
-
-    @Test
+    @DisplayName("Check current player is changed correctly")
     void nextPlayer() {
         assertEquals(game.getPlayers()[0], game.getCurrentPlayer());
-        for (int i = 0; i < 3; i++) {
-            game.nextPlayer();
-            assertEquals(game.getPlayers()[i + 1], game.getCurrentPlayer());
-        }
+        game.nextPlayer();
+        assertEquals(game.getPlayers()[1], game.getCurrentPlayer());
+        game.nextPlayer();
+        game.nextPlayer();
         game.nextPlayer();
         assertEquals(game.getPlayers()[0], game.getCurrentPlayer());
     }
 
-    // The game is ended if [gameEnder] is set and the round is complete
     @Test
+    @DisplayName("Game should be ended if [gameEnder] is set and the round is complete")
     void isEnded() {
-        assert((game.isEnded() && game.getGameEnder() != null && game.getCurrentPlayer() == game.getPlayers()[0]) ||
-                (!game.isEnded() && game.getGameEnder() == null));
+        assertFalse(game.isEnded());
+        game.setGameEnder(game.getPlayers()[0]);
+        assertTrue(game.isEnded());
     }
 
     @BeforeEach
@@ -88,17 +56,6 @@ class GameTest {
         Player player4 = new Player("andri", personalGoalCard2);
         Player[] players = new Player[]{player1, player2, player3, player4};
 
-        // Add tiles to players' bookshelf
-        try {
-            player1.getBookshelf().insertTiles(0, new ArrayList<Tile>(Arrays.asList(
-                    new Tile(Category.GAMES, Icon.VARIATION1, Orientation.UP),
-                    new Tile(Category.GAMES, Icon.VARIATION1, Orientation.UP),
-                    new Tile(Category.GAMES, Icon.VARIATION1, Orientation.UP)
-            )));
-        } catch (NotEnoughCellsException | PickTilesException e) {
-            throw new RuntimeException(e);
-        }
-
         // Create common goal cards
         CommonGoalCard commonGoalCard1 = new CommonGoalCard((b) -> true, players.length);
         CommonGoalCard commonGoalCard2 = new CommonGoalCard((b) -> false, players.length);
@@ -119,6 +76,6 @@ class GameTest {
 
         // Create game
         this.game = new Game(players, commonGoalCards, tiles, livingRoom);
-        game.setGameEnder(player1);
+
     }
 }
