c6f7ba63867e7963b1941a7c3da29b7adc2608d0
Fabio Sabbion <fabio.sabbion@mail.polimi.it>
2023-04-26 17:41:22 +0200

    Made classes serializable

--- src/main/java/it/polimi/ingsw/distributed/GameUpdate.java
+++ src/main/java/it/polimi/ingsw/distributed/GameUpdate.java
@@ -5,9 +5,10 @@ import it.polimi.ingsw.models.LivingRoom;
 import it.polimi.ingsw.models.Player;
 
 import javax.annotation.Nullable;
+import java.io.Serializable;
 import java.util.List;
 
 @Nullable
-public record GameUpdate(LivingRoom livingRoom, List<PlayerUpdate> players, List<CommonGoalCard> commonGoalCards, PlayerUpdate gameEnder, PlayerUpdate currentPlayer) {
-
+public record GameUpdate(LivingRoom livingRoom, List<PlayerUpdate> players, List<CommonGoalCard> commonGoalCards, PlayerUpdate gameEnder, PlayerUpdate currentPlayer) implements Serializable {
+    // TODO: invece di usare LivingRoom, bisognerebbe creare un oggetto immutabile, oppure mandare solo la matrice che rappresenta la board
 }
--- src/main/java/it/polimi/ingsw/models/Coordinates.java
+++ src/main/java/it/polimi/ingsw/models/Coordinates.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.models;
 
+import java.io.Serializable;
+
 /**
  * Simple auxiliary class that just contains two values, X and Y coordinates
  */
-public class Coordinates {
+public class Coordinates implements Serializable {
     public final int x;
     public final int y;
 
