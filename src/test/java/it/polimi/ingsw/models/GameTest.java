df7278862b0875388c10c1f0d1f37132a11669ae
lucapagano <luca1.pagano@mail.polimi.it>
2023-06-23 17:27:58 +0200

    Finished tests for BookShelf and Game

--- src/test/java/it/polimi/ingsw/models/GameTest.java
+++ src/test/java/it/polimi/ingsw/models/GameTest.java
@@ -71,6 +71,61 @@ class GameTest {
         this.game.emitGameState();
     }
 
+    @Test
+    @DisplayName("Checks for all get methods")
+    void getAndSetMethods() {
+        // Create personal goal cards
+        PersonalGoalCard personalGoalCard1 = new PersonalGoalCard(new ArrayList<>(Arrays.asList(
+                new MutablePair<>(Category.GAMES, new Coordinates(0, 0)),
+                new MutablePair<>(Category.BOOKS, new Coordinates(1, 1)),
+                new MutablePair<>(Category.PLANTS, new Coordinates(2, 2))
+        )
+        ),
+                "");
+        PersonalGoalCard personalGoalCard2 = new PersonalGoalCard(new ArrayList<>(Arrays.asList(
+                new MutablePair<>(Category.GAMES, new Coordinates(0, 1)),
+                new MutablePair<>(Category.BOOKS, new Coordinates(1, 2)),
+                new MutablePair<>(Category.PLANTS, new Coordinates(2, 4))
+        )
+        ),
+                "");
+
+        // Create players
+        Player player1 = new Player("fabio", personalGoalCard1);
+        Player player2 = new Player("lp", personalGoalCard2);
+        Player player3 = new Player("lorenzo", personalGoalCard1);
+        Player player4 = new Player("andri", personalGoalCard2);
+        Player[] players = new Player[]{player1, player2, player3, player4};
+
+        // Create common goal cards
+        CommonGoalCard commonGoalCard1 = new CommonGoalCard((b) -> true, players.length, 0);
+        CommonGoalCard commonGoalCard2 = new CommonGoalCard((b) -> false, players.length, 0);
+
+        CommonGoalCard[] commonGoalCards = new CommonGoalCard[]{commonGoalCard1, commonGoalCard2};
+
+        // Create living room
+        LivingRoom livingRoom = new LivingRoom();
+
+        // Create game
+        this.game = new Game(players, commonGoalCards, new ArrayList<>(), livingRoom, players[0]);
+
+
+        // Now I'll check each get
+        assertNull(this.game.getGameEnder());
+
+        this.game.setGameEnder(player1);
+        assertEquals(this.game.getGameEnder(), player1);
+
+        assertEquals(this.game.getPlayers(), players);
+        assertEquals(this.game.getLivingRoom(), livingRoom);
+        assertEquals(this.game.getRemainingTiles(), new ArrayList<>());
+        assertEquals(this.game.getCurrentPlayer(), players[0]);
+        assertEquals(this.game.getCommonGoalCards(), commonGoalCards);
+
+    }
+
+
+
     @BeforeEach
     void setup() {
         // Create personal goal cards
