eedc18aae1c0ba4811f73cd75c7d9bf08682632e
lucapagano <luca1.pagano@mail.polimi.it>
2023-06-28 15:38:39 +0200

    Added javaDoc to distributed

--- src/main/java/it/polimi/ingsw/distributed/PlayerUpdate.java
+++ src/main/java/it/polimi/ingsw/distributed/PlayerUpdate.java
@@ -6,7 +6,10 @@ import it.polimi.ingsw.models.Player;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.Serializable;
-
+/**
+ * The PlayerUpdate class represents an update of a player that can be transmitted over the network.
+ * It contains the player's nickname, bookshelf, and optional personal goal card update.
+ */
 @Nullable
 public record PlayerUpdate(@Nonnull String nickname, Bookshelf bookshelf, PersonalGoalCardUpdate personalGoalCard) implements Serializable {
     public static PlayerUpdate from(Player player, boolean includePersonalGoalCard) {
